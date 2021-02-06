767e846173009f5d107ca1756b8e7662643e56a3
ClimbSnail <hqclimb@gmail.com>
2021-02-06 21:20:08 +0800

    整合

---
+++ Software/knobs.py
@@ -0,0 +1,116 @@
+from machine import Pin
+import gc
+
+"""
+开关编码器功能文件
+"""
+
+"""
+外部中断mode：
+        Pin.IN  输入
+        Pin.OUT 输出
+      value：输出电平
+      Pin.value（[value]）不带参数时是读取输入电平，带参数时是设置输出电平,参数是1/0.
+      Pin.irq(*,trigger, handler=None)
+中断irq接口
+      trigger，触发方式(不指定 默认为上升下降沿)
+      Pin.IRQ_FALLING，下降沿
+      Pin.IRQ_RISING，上升沿
+      Pin.IRQ_RISING | Pin.IRQ_FALLING，上升下降沿
+      handler，回调函数
+"""
+
+
+
+
+class EC11:
+    def __init__(self, pinA_num, pinB_num, pinSw_num=None):
+        """
+        编码器类的初始化   注：在大多数情况下，只能使用引脚0、2、4、5、12、13、14、15和16
+        :param pinA_num: 接收编码器脉冲的引脚A
+        :param pinB_num: 接收编码器脉冲的引脚B
+        :param pinSw_num: 按键触发的引脚（此为可选参数）
+        """
+        self.pinA_Status = 0
+        self.pinB_Status = 0
+        self.pulse_count = 0
+        self.switch_status = 0
+        
+        #self.cnt_value = 0  # 编码器距离上一轮的脉冲增量
+        self.pinA = Pin(pinA_num, Pin.IN, Pin.PULL_UP)
+        self.pinA.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.interruter_funcA)
+        self.pinB = Pin(pinB_num, Pin.IN, Pin.PULL_UP)
+        self.pinB.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self.interruter_funcB)
+        self.pinSw = None
+        if pinSw_num != None:
+            self.pinSw = Pin(pinSw_num, Pin.IN, Pin.PULL_UP)
+            self.pinSw.irq(trigger=Pin.IRQ_FALLING, handler=self.interruter_funcSW)
+        gc.collect()
+
+    def interruter_funcA(self, events):
+        """
+        发生外部中断后执行的函数
+        :return:
+        """
+        self.pinA_Status = self.pinA.value()
+        if self.pinA_Status == 1 and self.pinB_Status == 0:
+            self.pulse_count += 1
+        gc.collect()
+
+    def interruter_funcB(self, events):
+        """
+        发生外部中断后执行的函数
+        :return:
+        """
+        self.pinB_Status = self.pinB.value()
+        if self.pinB_Status == 1 and self.pinA_Status == 0:
+            self.pulse_count -= 1
+        gc.collect()
+
+    def interruter_funcSW(self, events):
+        """
+        发生外部中断后执行的函数
+        :return:
+        """
+        self.switch_status = 1
+
+    def get_data(self):
+        """
+        获取本次检测到的脉冲数量
+        :return: 距离上一次获取所产生的脉冲数、按键状态
+        """
+        pulse = self.pulse_count
+        self.pulse_count = 0	# clear
+        sw_status = self.switch_status
+        self.switch_status = 0	# clear
+        gc.collect()
+        return pulse, sw_status
+
+
+"""
+This is demo
+"""
+#if __name__ == "test":
+if __name__ == "__main__":
+    import gc
+    gc.collect()
+    knobs = EC11(4, 5, 0)
+    import time
+    while True:
+        print( knobs.get_data() )
+        time.sleep(0.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Software/main.py
@@ -0,0 +1,239 @@
+
+
+import network
+import time
+from wifi import WifiController
+from knobs import EC11
+from max6675 import MAX6675
+from ssd1306_esp import SSD1306_I2C
+from pid import PID
+import webrepl
+from machine import Pin, PWM, I2C
+import machine
+from machine import Timer
+import gc
+
+class Interface():
+  def __init__(self, scl, sda):
+    i2c = I2C(scl=scl, sda=sda, freq=400000)
+    self.oled = SSD1306_I2C(128, 64, i2c, addr=0x3c, external_vcc=False)
+    self.set_draw_panel()
+    self.show()
+	
+  def set_draw_panel(self):
+    weigth = 2
+    self.oled.fill_rect(0, 0, 128, weigth, 1)
+    self.oled.fill_rect(0, 0, weigth, 64, 1)
+    self.oled.fill_rect(126, 0, weigth, 64, 1)
+    self.oled.fill_rect(0, 62, 128, weigth, 1)
+    tu = b"\x00\x80\xC0\xE0\xF8\xFF\xFE\xFC\x70\x00\x00\xC0\x80\x00\x00\x00\x00\x1F\x3F\x7F\x9F\x07\x03\xE1\xF0\xF8\xFE\xFF\xFF\x7F\x3C\x00"
+    self.oled.img_16x16(tu, 107, 3)
+    tu = b"\x00\x00\x00\x80\xE0\xF8\xFC\x0E\x0E\xFC\xF8\xE0\x80\x00\x00\x00\x00\x38\x7E\x7F\x7F\x7F\x7F\x66\x66\x7F\x7F\x7F\x7F\x7E\x38\x00"
+    self.oled.img_16x16(tu, 107, 20)
+    #tu = b"\x00\x00\x00\x80\xE0\xF8\xFC\x0E\x0E\xFC\xF8\xE0\x80\x00\x00\x00\x00\x38\x7E\x7F\x7F\x7F\x7F\x66\x66\x7F\x7F\x7F\x7F\x7E\x38\x00"
+    #self.oled.img_16x16(tu, 118, 40)
+    #self.oled.fill_rect(0,  31, 96, 2, 1)
+    
+  def set_temperature_target(self, dat_str):
+    self.oled.text("SET: ", 3, 3)
+    self.oled.fill_rect(30, 3, 4*8, 8, 0)
+    self.oled.text(dat_str, 30, 3)
+        
+  def set_temperature_now(self, dat_str):
+    self.oled.fill_rect(30, 24, 4*8, 16, 0)
+    self.oled.text_num(dat_str, 30, 24)
+     
+  def set_pwm(self, dat_str):
+    self.oled.text("PWM: ", 3, 52)
+    self.oled.fill_rect(30, 52, len(dat_str)*8, 8, 0)
+    self.oled.text(dat_str, 30, 52)
+
+  def show(self):
+    self.oled.show()
+
+class HeatPlatform():
+  """
+  加热平台控制主类
+  """
+  def __init__(self, config_dict, LED_Status = True):
+    self.cfg = config_dict # 配置文件信息
+    self.pwm_out = PWM(Pin(2), freq=5, duty=1023)
+    # self.pwm_out = Pin(2, Pin.OUT)
+    # self.pwm_out.on()
+    
+    self.max6675 = MAX6675(Pin(0), None, Pin(13), Pin(16, Pin.OUT))
+    
+    self.temp_now = self.max6675.read_temperature()
+    self.temp_target = 50
+    self.temp_DValue = 3
+    self.pid_enable = False
+    gc.collect()
+    
+    # 创建OLED显示器
+    # construct an I2C bus，软件i2c所有接口都适用
+    self.LED_STATUS = LED_Status
+    if self.LED_STATUS == True:
+      self.interface = Interface(scl=Pin(1), sda=Pin(3))
+      self.interface.set_temperature_now( str(self.temp_now) )
+      self.interface.set_temperature_target( str(self.temp_target) )
+      self.interface.show()
+	  
+    
+    # 创建编码器控制对象
+    self.knobs = EC11(4, 5, 0)
+    
+    # 创建pid控制对象
+    self.pid_contorller = PID(0.7, 0.4, 0.25, 0.5)
+    
+    # 创建按键检测的定时器
+    self.knobs_timer = Timer(-1)  # periodic with 100ms period
+    # mode: Timer.ONE_SHOT / Timer.PERIODIC
+    self.knobs_timer.init(period=1000, mode=Timer.PERIODIC, callback=self.check_knobs)
+    
+    # 创建pid调节的定时器
+    self.adjust_timer = Timer(-1)
+    # mode: Timer.ONE_SHOT / Timer.PERIODIC
+    self.adjust_timer.init(period=500, mode=Timer.PERIODIC, callback=self.adjust_temperature)
+    
+    gc.collect()
+  
+  def check_knobs(self, timer):
+    increase = self.knobs.get_data()
+    # increase为一个长度为2的元组 编码脉冲+按键是否按下 
+    #if increase[1] == 1:
+    #  if self.pwm_out.value() == 1:
+    #    self.pwm_out.off()
+    #  else:
+    #    self.pwm_out.on()
+    if increase[0] != 0:
+      self.temp_target = self.temp_target + increase[0]
+      
+      if self.LED_STATUS == True:
+        self.interface.set_temperature_target( str(self.temp_target) )
+        self.interface.show()
+  
+  def adjust_temperature(self, timer):
+    self.temp_now = self.max6675.read_temperature()
+    str_temp_now = str(self.temp_now)
+    pwm_val = None  # 输出的PWM值
+    print("\ntemp_now = "+str_temp_now)
+    if self.temp_target - self.temp_now > self.temp_DValue:
+      pwm_val = 0
+      self.pid_enable = False
+    elif self.temp_now > self.temp_target:
+      pwm_val = 1023
+      self.pid_enable = False
+    else: 
+      if self.pid_enable == False:
+        self.pid_enable = True
+        self.pid_contorller.clear_data()
+      out_val = self.pid_contorller.get_output(self.temp_target, self.temp_now)
+      str_out = str(out_val)
+    
+      print("str_out = "+str_out)
+      pwm_val = int(out_val)*100
+      if pwm_val>0:
+        if pwm_val>1020:
+          pwm_val = 0
+        else:
+          pwm_val = 1024-pwm_val
+      else:
+        pwm_val = 1023
+    self.pwm_out.duty(pwm_val)
+    print("pwm_val = "+str(pwm_val))
+    
+    if self.LED_STATUS == True:
+      if str_temp_now == "4095":
+        str_temp_now = "XXXX"
+      self.interface.set_temperature_now(str_temp_now)
+      self.interface.set_pwm(str(pwm_val))
+      self.interface.show()
+    
+
+  def __del__(self):
+    self.knobs_timer.deinit()
+    self.adjust_timer.deinit()
+    print("Stop HeatPlatform")
+    
+    
+
+class MyBoot():
+  def __init__(self, config_dict):
+    self.cfg = config_dict # 配置文件信息
+    webrepl.start(password = self.cfg['webrepl-password'])
+    # self.wifi = WifiController()
+    # self.connect(self.cfg)
+    # self.disconnect(self.cfg)
+    # create and configure in one go
+  
+  def disconnect(self, cfg_dict):
+    ret = self.wifi.get_valid_bs_list(["orangepi"])
+    print(ret)
+    self.wifi.close_sta()
+    print("close_sta succ")
+    self.wifi.close_ap()
+    print("close_ap succ")
+      
+  def connect(self, cfg_dict):
+    ret = self.wifi.get_valid_bs_list(["orangepi"])
+    print(ret)
+    #ret = conn.do_sta("Test", "88888888")
+    ret = self.wifi.do_sta(cfg_dict["sta-essid"], cfg_dict["sta-password"])
+    if ret == True:
+      print("do_sta succ")
+    ret = self.wifi.create_ap (cfg_dict["ap-essid"], cfg_dict["ap-password"])
+    if ret == True:
+      print("do_ap succ")
+    
+def is_ADC_dress():
+  from machine import ADC
+  adc = ADC(0)            # create ADC object on ADC pin
+  if  adc.read()<512:
+    return False
+  else:
+    return True
+
+
+"""
+This is demo
+"""
+#if __name__ == "test":
+if __name__ == '__main__':
+	import gc
+	gc.collect()
+	import json
+	cfg = None
+	with open('config.json', 'r') as f:
+	  cfg = json.loads(f.read())
+	LED_STATUS = is_ADC_dress()
+	#LED_STATUS = False
+	print(LED_STATUS)
+	hp = HeatPlatform(cfg, LED_STATUS)
+	#boot = MyBoot(cfg)
+	# os.remove('wifi_config.json') # 鍒犻櫎鏂囦欢
+	#webrepl.start(password = cfg['webrepl-password'])
+	while True:
+	  time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
